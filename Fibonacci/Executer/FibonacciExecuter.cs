3504314399e864b5c0d32e59eafb4d5ff3a97ab2
Larissa Chiaradia <lari.chiaradia.silva@gmail.com>
2022-07-28 22:54:15 -0300

    feat: add Fibonacci Executer.

---
+++ Fibonacci/Executer/FibonacciExecuter.cs
@@ -0,0 +1,22 @@
+﻿namespace Fibonacci.Executer
+{
+    public static class FibonacciExecuter
+    {
+        public static bool IsFibonacciNumber(int number)
+        {
+            int num1 = 0, num2 = 1, aux;
+
+            for (int i = 0; i​​​​​​​ <= number; i++)
+            {
+                aux = num1;
+                num1 = num2;
+                num2 = num1 + aux;
+
+                if (number == num1 || number == num2 || number == aux)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
